ee33d08fd58c95c930e65f209530ff8de98a6d2d
Simrit <kaulsimrit@gmail.com>
2024-05-18 12:54:00 +0530

    Basic background set + introduced the header component

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="z-[999] relative">
+      <div className="fixed top-0 left-1/2 h-[5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-50"></div>
+    </header>
+  );
+};
+
+export default Header;
