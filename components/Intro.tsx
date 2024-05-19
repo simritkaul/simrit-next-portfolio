e889da36faa5bf2f64613ed80464986917d4ddb4
Simrit <kaulsimrit@gmail.com>
2024-05-19 13:05:28 +0530

    Intro done

---
+++ components/Intro.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+const Intro = () => {
+  return (
+    <>
+      <section className="mb-28 max-w-[64rem]">
+        <div className="flex items-center justify-center">
+          <div className="relative">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "tween",
+                duration: "0.25",
+              }}
+            >
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=3280&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+                alt="Simrit Photo"
+                width={"192"}
+                height={"192"}
+                quality={"95"}
+                priority={true}
+                className="h-42 w-42 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              />
+            </motion.div>
+          </div>
+        </div>
+
+        <motion.p
+          className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl relative"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <span className="font-bold">Hello, I'm Simrit.</span> I'm a{" "}
+          <span className="font-bold">full-stack developer</span> with{" "}
+          <span className="font-bold">2 years</span> of experience. I enjoy
+          building <span className="italic">websites and apps</span>. My focus
+          is <span className="underline">React (Next.js)</span>.
+        </motion.p>
+
+        <motion.div
+          className="flex flex-col sm:flex-row justify-center gap-4 px-4 text-lg font-medium"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.1,
+          }}
+        >
+          <Link
+            href={"#contact"}
+            className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          >
+            Contact me here{" "}
+            <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          </Link>
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+            href="/Simrit_Resume_13Apr24.pdf"
+            download={true}
+          >
+            Download Resume{" "}
+            <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+            href="https://www.linkedin.com/in/simrit-kaul/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+            href="https://github.com/simritkaul"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </a>
+        </motion.div>
+      </section>
+    </>
+  );
+};
+
+export default Intro;
