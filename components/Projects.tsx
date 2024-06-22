c86d663f98e703b6238d6b2b4546a9b2287547bc
Simrit <kaulsimrit@gmail.com>
2024-06-22 18:56:40 +0530

    Changes for finishing the header section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,11 +5,14 @@ import { motion } from "framer-motion";
 import SectionHeading from "./SectionHeading";
 import { projectsData } from "@/lib/data";
 import Project from "./Project";
+import { useSectionInView } from "@/lib/hooks";
 
 const Projects = () => {
+  const { ref } = useSectionInView("Projects");
+
   return (
     <>
-      <motion.section>
+      <motion.section ref={ref} id="projects" className="scroll-mt-28">
         <SectionHeading>My Projects</SectionHeading>
         <div>
           {projectsData?.map((project) => (
